aefa354a4e1da4b9b1759578609a431fee71c954
csdevignes <159613482+csdevignes@users.noreply.github.com>
2024-02-17 10:30:55 +0100

    Addition of README, LICENSE and command line usage.

---
+++ README.md
@@ -0,0 +1,36 @@
+# Hangman game - Jeu du pendu
+
+## Description - EN:
+This program allows playing Hangman game. A word is picked
+randomly from a dictionary / lexicon.  The user then enters
+letter to try and guess the words. 10 mistakes are allowed.
+By default a french lexicon is used. But it is possible to
+use a custom lexicon (see usage).
+
+## Description - FR:
+Ce programme permet de jouer au jeu du pendu.
+Un mot à deviner est tiré aléatoirement à partir d'un
+lexique. L'utilisateur entre ensuite des lettres pour
+essayer de deviner le mot.
+
+Il est possible d'utiliser un lexique de mots personnalisé (cf usage.)
+
+## TODO
+* Add the possibility to play again and add a game counter
+* Provide an english lexicon
+
+
+## Usage du programme:
+```console
+python pendu.py [-l <lexicon.txt>]
+python pendu.py [-lexicon <lexicon.txt>]
+```
+> The default lexicon (in french) is provided in [dico.txt](dico.txt)
+
+
+## Crédits:
+Institut des sciences du Digital, Management & Cognition – IDMC
+Certification Data Scientist, class of Algorithm
+
+## Licence:
+MIT License
--- pendu.py
+++ pendu.py
@@ -7,24 +7,31 @@ python pendu.py
 '''
 
 import functions as fct
+import argparse
 
-lexicon_name = "dico.txt"
+# Define argument for usage in command line
+parser = argparse.ArgumentParser(description="Argument analysis")
+parser.add_argument('-l', '--lexicon', help="Path to word dictionary/lexicon.")
+# Argument analysis
+args = parser.parse_args()
+lexicon_name = args.lexicon
+# If no lexicon is defined, load default file
+if not lexicon_name:
+    lexicon_name = "dico.txt"
 
-if __name__ == "__main__":
-    word = fct.word_pick(lexicon_name)
-    letter = ''
-    tried = ''
-    wrong = 0
-    print(word)
+word = fct.word_pick(lexicon_name)
+letter = ''
+tried = ''
+wrong = 0
 
-    table = list()
-    for i in range(0, len(word)):
-        table.append(0)
-    k = 0
-    while k == 0:
-        fct.display_word(word, table)
-        tried, letter = fct.enter_letter(tried)
-        table, wrong = fct.check_letter(letter, word, table, wrong)
-        k = fct.end_game(table, wrong)
-    fct.epilogue(word, k)
+table = list()
+for i in range(0, len(word)):
+    table.append(0)
+k = 0
+while k == 0:
+    fct.display_word(word, table)
+    tried, letter = fct.enter_letter(tried)
+    table, wrong = fct.check_letter(letter, word, table, wrong)
+    k = fct.end_game(table, wrong)
+fct.epilogue(word, k)
 
